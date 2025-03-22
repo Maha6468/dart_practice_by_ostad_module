ea60a26bf9208fca4bf63b5ba0dbf60d6d4a0477
Maha6468 <motherma7633@gmail.com>
2025-03-23 02:38:23 +0600

    .

---
+++ bin/lib/Module_1/class3.dart
@@ -0,0 +1,36 @@
+void main()
+{
+  final int age=25;   // runtime , mane ata jekono somoy man ba value ta assign korte parbo
+  const double pi=3.1416;  // compile time,  ata sate sate value assign korte hbe
+  print(age);
+  print(pi);
+
+  final DateTime nowTime=DateTime.now();
+  print(nowTime);
+
+
+  //Assignment operator
+  int a=5;
+  a+=3;
+  print(a);
+  a-=2;
+  print(a);
+  a*=2;
+  print(a);
+
+  //Relational Operator
+  int x=10,y=-20;
+  print(x<y);
+  print(x>y);
+  print(x==y);
+
+  //logical operator
+  print((x>5)&&(y>20));
+  print((x>5)||(y>20));
+  print(!(x==y));
+
+
+
+
+
+}
