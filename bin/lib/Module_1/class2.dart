f50d1dbbc8664ade91166ce0fca2a93df20c518e
Maha6468 <motherma7633@gmail.com>
2025-03-23 00:24:12 +0600

    .

---
+++ bin/lib/Module_1/class2.dart
@@ -0,0 +1,34 @@
+void main()
+{
+String firstName='Mahabubar';
+String lastName='Rahman';
+String fullName='$firstName $lastName';
+print(fullName);
+print(fullName.length);
+print(firstName.toUpperCase());
+print(lastName.toLowerCase());
+print(fullName.contains('Mahabubar'));
+
+
+
+var person=10;
+//var person='Maha'; //not allow
+print(person);
+person=12;
+print(person);
+print(person.runtimeType);
+
+dynamic person2='Mariya';
+print(person2);
+person2=44;
+print(person2);
+print(person2.runtimeType);
+person2=true;
+print(person2);
+print(person2.runtimeType);
+
+
+
+
+
+}
