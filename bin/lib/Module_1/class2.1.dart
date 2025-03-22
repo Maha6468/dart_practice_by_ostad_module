096b24134f7ed6ff35f99a2cfaa1cc57be024ab1
Maha6468 <motherma7633@gmail.com>
2025-03-23 00:43:44 +0600

    .

---
+++ bin/lib/Module_1/class2.1.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  int num1=10;
+  int num2=20;
+
+  print("Sum: $num1+$num2=${num1+num2}");
+  print("Sub: $num1-$num2=${num1-num2}");
+  print("Mul: $num1*$num2=${num1*num2}");
+  print("Divi: $num1/$num2=${num1/num2}");
+
+  String num3='25';
+  print("Sum: $num1+$num3=${num1+int.parse(num3)}");
+
+}
