6c143d2941dd47ed1bfa0c1bb6815c042974ca0b
Maha6468 <motherma7633@gmail.com>
2025-03-22 23:47:48 +0600

    print process

---
+++ bin/lib/Module_1/class1.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  int number_1=20;
+  String name='Maha';
+  bool isTrue=false;
+  double number_2=10.26;
+
+  print(number_1);
+  print(name);
+  print(isTrue);
+  print(number_2);
+
+  print('$number_1 $name $isTrue $number_2');
+
+
+}
