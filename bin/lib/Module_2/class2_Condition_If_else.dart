25768edf4c5097e83d3bc9a02e9e0f31d49acf3c
Maha6468 <motherma7633@gmail.com>
2025-03-23 23:17:26 +0600

    condition

---
+++ bin/lib/Module_2/class2_Condition_If_else.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  var amount=700;
+
+  if(amount>=50){
+    print("ami friend der sate share a jabo");
+    }else if(amount>100){
+    print("ami bike a jabo");
+  }else if(amount>=200){
+    print("ami rskw jabo");
+  }
+  else{
+    print("ame akai jabo");
+  }
+
+
+
+}
