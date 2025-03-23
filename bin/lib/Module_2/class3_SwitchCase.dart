f8f91986726c760b3c0cfe8f228133b30e94fe64
Maha6468 <motherma7633@gmail.com>
2025-03-24 04:55:17 +0600

    switch case

--- bin/lib/Module_2/class3_SwitchCase.dart
+++ bin/lib/Module_2/class3_SwitchCase.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 void main()
 {
 print("Enter Day:");
+/*
 String? day=stdin.readLineSync();
 switch(day){
   case 'far':
@@ -19,6 +20,23 @@ switch(day){
   default:
     print('basay');
 }
+
+ */
+  int month=1;
+  switch(month){
+    case 12 || 1 ||2 :
+      print("winter");
+      break;
+    case 3||4||5:
+      print('spring');
+      break;
+    case 6 || 7 || 8:
+      print("summer");
+      break;
+    case 9||10||11:
+      print('autum');
+      break;
+  }
 
 
 
