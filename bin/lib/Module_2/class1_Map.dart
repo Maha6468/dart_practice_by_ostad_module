6ff855bd45e81dcebaf5bfa2c1648d9b438590a7
Maha6468 <motherma7633@gmail.com>
2025-03-23 16:16:24 +0600

    Map

--- bin/lib/Module_2/class1_Map.dart
+++ bin/lib/Module_2/class1_Map.dart
@@ -1,5 +1,46 @@
 void main()
 {
+  var person={
+    'name': 'Taufiq',
+    'age': 26,
+    'expe': '7 years'
+  };
+  print(person);
+  print(person["name"]);
+  print(person["age"]);
+  person['address']='Dhaka';
+  print(person);
+  person['age']=29;
+  print(person);
+  person.remove('expe');
+  print(person);
+  print(person.containsKey('name'));
+  print(person.containsValue('Dhaka'));
+  print(person.keys);
+  print(person.values);
+  print(person.length);
+
+
+  var additionalInfo={
+    'Subject': 'cse',
+    'CGPA': 3.80
+  };
+  person.addAll(additionalInfo);
+  print(person);
+
+
+  var KayList=person.keys.toList();
+  var ValueList=person.values.toList();
+  print(KayList);
+  print(ValueList);
+
+
+
+
+
+
+
+
 
 
 }
