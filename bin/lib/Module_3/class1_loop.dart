3864584cbe7eff2e8387a3ff971807523d9ff3d9
Maha6468 <motherma7633@gmail.com>
2025-03-25 00:58:52 +0600

    loop

---
+++ bin/lib/Module_3/class1_loop.dart
@@ -0,0 +1,62 @@
+main()
+{
+  /*
+  //for loop
+  for(int i=1;i<=10;i++){
+    print("ami nice geci $i time");
+  }
+
+
+  for(int i=1;i<=10;i++){
+    print("15*$i=${15*(i)}");
+  }
+
+
+  //list implement in loop
+  List<String>student=['karim','rahim','taufiq','jojo','rafiq'];
+  List<int> amount=[1000,500,750,600,800];
+  //print("student 2 index: ${student[2]}");
+
+
+  for(int index=0;index<=student.length;index++){
+    print('Student $index name:${student[index]}\n');
+    print('your ${amount[index]} payment due');
+  }
+
+   */
+
+  //forEach
+
+
+  /*
+  //while loo[
+  int i=5;
+  while(i<=10){
+    print("hello my index is $i");
+    i++;
+  }
+
+   */
+
+
+  //list implement by while loop
+
+
+  //do while loop
+  // list implement by do while loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
