52327e7e5e19d0eff2f62f17c2eb606fd0ee5193
Maha6468 <motherma7633@gmail.com>
2025-03-25 11:37:55 +0600

    .

---
+++ bin/lib/Module_3/class2_function.dart
@@ -0,0 +1,25 @@
+   void main()
+   {
+     double length1=.5;
+     double width1=.8;
+     double area1=length1*width1;
+     print("area1=$area1");
+
+
+     double length2=.25;
+     double width2=.5;
+     double area2=length2*width2;
+     print("area1=$area2");
+
+
+     double length3=.9;
+     double width3=.58;
+     double area3=length3*width3;
+     print("area1=$area3");
+
+
+
+
+
+
+   }
