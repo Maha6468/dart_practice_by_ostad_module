1a0e4cba5f86275725ac53ff704a74feb6e6374b
Maha6468 <motherma7633@gmail.com>
2025-03-25 12:01:44 +0600

    function

--- bin/lib/Module_3/class2_function.dart
+++ bin/lib/Module_3/class2_function.dart
@@ -1,5 +1,7 @@
    void main()
    {
+
+     //without function
      double length1=.5;
      double width1=.8;
      double area1=length1*width1;
@@ -9,17 +11,76 @@
      double length2=.25;
      double width2=.5;
      double area2=length2*width2;
-     print("area1=$area2");
+     print("area2=$area2");
 
 
      double length3=.9;
      double width3=.58;
      double area3=length3*width3;
-     print("area1=$area3");
+     print("area3=$area3");
+
+
+     //with function
+     print("area1 with function:${calculateArea(.5,.8)}");
+     print("area2 with function:${calculateArea(.25,.5)}");
+     print("area3 with function:${calculateArea(.9,.58)}");
 
 
 
+     print("area1 with function:${calculateArea1(.5,.8)}");
+     print("area2 with function:${calculateArea1(.25,.5)}");
+     print("area3 with function:${calculateArea1(.9,.58)}");
 
+
+
+     calculateArea_with_prm(.5, .7);
+     calculateArea_with_prm(.5, .7,'Dhaka');
+     calculateArea_with_prm(.5, .7,'Rangpur');
 
 
    }
+
+   //1way
+   double calculateArea(double length, double width){
+  double area=length*width;
+  return area;
+   }
+
+   //2way
+   var calculateArea1=(double length,double width)=>(length*width);
+
+
+
+
+    //3way optional peramiter
+    calculateArea_with_prm(double length, double width,[String?des]){
+     double area=length*width;
+     if(des!=null){
+       print("$des==$area");
+     }
+     else{
+       print("only area=$area");
+     }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
