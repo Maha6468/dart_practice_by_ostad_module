4bf4b03587d7d46f170ba6a82968608f8402176c
Maha6468 <motherma7633@gmail.com>
2025-03-25 14:33:07 +0600

    class

---
+++ bin/lib/Module_3/class3_class.dart
@@ -0,0 +1,50 @@
+main(){
+Student student1=Student();
+student1.studentname='Rahim';
+student1.studentclass='class 10';
+student1.stdunrtAddress='Dhaka';
+
+
+print(student1.studentname);
+print(student1.studentclass);
+print(student1.stdunrtAddress);
+
+
+Human maruf=Human();
+print(maruf.legs);
+print(maruf.hands);
+print(maruf.eye);
+
+
+Human rahim=Human();
+rahim.hands=1;
+rahim.legs=3;
+rahim.eye=4;
+print("Rahim have:${rahim.hands} hand,${rahim.legs} legs,${rahim.eye} eys");
+
+
+
+
+
+
+
+}
+
+
+//class
+class Student{
+  String? studentname;
+  String? studentclass;
+  String? stdunrtAddress;
+}
+
+
+class Human{
+  int legs=2;
+  int hands=2;
+  int eye=2;
+}
+
+
+
+//method......dekhte hbe
