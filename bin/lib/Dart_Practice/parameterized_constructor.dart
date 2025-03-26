b0d3edafa91d0aa3a328074286edb62eb1aa4e22
Maha6468 <motherma7633@gmail.com>
2025-03-26 23:18:38 +0600

    constructor...

---
+++ bin/lib/Dart_Practice/parameterized_constructor.dart
@@ -0,0 +1,24 @@
+class Person {
+  String name;
+  int age;
+
+  // Parameterized Constructor
+  //worng
+  /*
+  Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+   */
+  Person(this.name, this. age);
+
+  void display() {
+    print("👤 Name: $name, Age: $age");
+  }
+}
+
+void main() {
+  Person p1 = Person("John", 25);
+  p1.display();
+}
