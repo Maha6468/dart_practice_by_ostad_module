d6d5ba30622dbc015fbc5277b6be1e6d7ee2b712
Maha6468 <motherma7633@gmail.com>
2025-03-26 22:58:57 +0600

    constructor..

---
+++ bin/lib/Dart_Practice/Default_constructor_with _initialize.dart
@@ -0,0 +1,18 @@
+class Car {
+  String brand = "Toyota";
+  int year = 2022;
+
+  // Default Constructor
+  Car() {
+    print("Car Object Created!");
+  }
+
+  void display() {
+    print("Brand: $brand, Year: $year");
+  }
+}
+
+void main() {
+  Car car1 = Car();
+  car1.display();
+}
