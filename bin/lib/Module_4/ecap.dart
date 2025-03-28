b3514cbc45f2cd5b4631c3014d98d750a9733efe
Maha6468 <motherma7633@gmail.com>
2025-03-29 01:58:53 +0600

    Encapsulation

---
+++ bin/lib/Module_4/ecap.dart
@@ -0,0 +1,36 @@
+ class Animal{
+  String name;
+  static var test='this is animal class';
+  Animal(this.name);
+
+  eat(){
+    print('$name is eating');
+  }
+
+  speak(){
+    print('$name is speaking');
+  }
+
+ }
+
+
+ class Dog extends Animal{
+  @override
+  speak(){
+    print('gue gue');
+  }
+  String color;
+  Dog(this.color,String name):super(name);
+
+ }
+
+main(){
+  //static use
+  print(Animal.test);
+
+  Dog tom=Dog('Black','tom');
+  tom.speak();
+
+
+}
+
