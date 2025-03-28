3e8ca9b76097579f309a1865a0bbf5e1426d61c2
Maha6468 <motherma7633@gmail.com>
2025-03-28 12:02:01 +0600

    constructor with list..

--- bin/lib/Dart_Practice/List_Use_With_Constructor.dart
+++ bin/lib/Dart_Practice/List_Use_With_Constructor.dart
@@ -1,4 +1,4 @@
-class ShoppingList{
+/*class ShoppingList{
   List<String> items;
   ShoppingList(this.items);
   void display(){
@@ -10,3 +10,42 @@ void main(){
   ShoppingList list=ShoppingList(['Milk','bread','egg']);
   list.display();
 }
+
+ */
+
+class Book{
+ String title;
+ List<String> authors;
+  Book(this. title,this. authors);
+
+  void display(){
+    print("Book :$title ,and Authors:$authors");
+  }
+
+}
+class ShoppingList{
+  List<String> items;
+  ShoppingList(this.items);
+  void display(){
+    print("My shopping list is: $items");
+  }
+
+}
+void main(){
+  Book book=Book('Dart programming book', ['John','Alice']);
+  book.display();
+  ShoppingList list=ShoppingList(['Milk','bread','egg']);
+  list.display();
+}
+
+
+
+
+
+
+
+
+
+
+
+
