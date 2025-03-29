c68ab12463be0649371485616a2dbc02ed9ebf38
Maha6468 <motherma7633@gmail.com>
2025-03-30 00:44:38 +0600

    encapsulation3

---
+++ bin/lib/Dart_Practice/encapsulation_3_with_BankAccount.dart
@@ -0,0 +1,15 @@
+class BankAccount{
+  String _accountNumber;
+  int _balance;
+
+  BankAccount(this._accountNumber,this._balance);
+
+  String getAccountName()=>_accountNumber;
+  int getBalance()=>_balance;
+
+}
+
+void main(){
+  BankAccount BA=BankAccount('6468', 500);
+  print("Account name is :${BA._accountNumber} \nBalance have:${BA._balance}");
+}
