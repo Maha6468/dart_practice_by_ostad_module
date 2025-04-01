65537179270d583f28e836e474f5e8a0b0602fb7
Maha6468 <motherma7633@gmail.com>
2025-04-01 14:13:00 +0600

    polymorphism super keyword

---
+++ bin/lib/Dart_Practice/Polymorphism_with_superkeyword.dart
@@ -0,0 +1,18 @@
+class Animal {
+  void makeSound() {
+    print("Animals make sounds");
+  }
+}
+
+class Cat extends Animal {
+  @override
+  void makeSound() {
+    super.makeSound();              //look
+    print("Cat meows: Meow Meow!");
+  }
+}
+
+void main() {
+  Cat obj = Cat();
+  obj.makeSound();
+}
